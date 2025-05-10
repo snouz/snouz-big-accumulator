0cf09f727bae05f497bff39c8b5bb310fd7a3a00
snouz <snooz-x@hotmail.com>
2025-05-10 09:23:51 +0200

    1.0.4

--- data-updates.lua
+++ data-updates.lua
@@ -1,3 +1,4 @@
 if mods["space-age"] then
   data.raw.recipe["big-accumulator"].category = "electronics"
+  data.raw.item["big-accumulator"].default_import_location = "fulgora"
 end
--- data.lua
+++ data.lua
@@ -112,7 +112,7 @@ data:extend(
     drop_sound = item_sounds.electric_large_inventory_move,
     place_result = "big-accumulator",
     stack_size = 20,
-    weight = 1000 * kg
+    weight = 200 * kg
   },
 
   {
